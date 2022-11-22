40a81b06e7cb8f942eab996053f9715cf23e4cc4
PaulineVarin <PaulineVarin@172.17.152.31>
2022-11-22 15:23:48 +0100

    Initialisation suite

--- src/petitPrince/PetitPrince0.java
+++ src/petitPrince/PetitPrince0.java
@@ -4,11 +4,15 @@ import asteroide.Sujet;
 import asteroide.console.*;
 
 public class PetitPrince0 implements Sujet{
-	private String nom;
-	private int argent;
+	private String nom = "Petit prince non joueur" ; 
+	private int argent = 100;
 	private ConsoleJavaBoy console ; 
 	private int cpt ; 
 	
-	public PetitPrince0
+	public PetitPrince0() {
+		
+	}
+	
+	public String 
 
 }
