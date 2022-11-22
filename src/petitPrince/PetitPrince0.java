f75ade9e45498bcfa37dd29e7001f0bbc8f2bfcb
PaulineVarin <PaulineVarin@172.17.152.31>
2022-11-22 15:28:26 +0100

    Mise à jour PetitPrince0

--- src/petitPrince/PetitPrince0.java
+++ src/petitPrince/PetitPrince0.java
@@ -4,15 +4,33 @@ import asteroide.Sujet;
 import asteroide.console.*;
 
 public class PetitPrince0 implements Sujet{
-	private String nom = "Petit prince non joueur" ; 
-	private int argent = 100;
+	private String nom ;  
+	private int argent ; 
 	private ConsoleJavaBoy console ; 
 	private int cpt ; 
 	
 	public PetitPrince0() {
+		this.nom = "Petit prince non joueur" ;
+		this.argent = 100 ; 
+		this.cpt = 0 ; 
+		this.console = new ConsoleJavaBoy(this) ;
 		
 	}
 	
-	public String 
+	public String getNom() {
+		return this.nom ; 
+	}
+	
+	public int getArgent() {
+		return this.argent ; 
+	}
+	
+	public void run() {
+		this.cpt++ ; 
+		this.console.parler("[tour"+cpt+"] "+ " Dessine moi un mouton");
+		this.console.seDirigerVers(0);
+	}
+	
+	
 
 }
