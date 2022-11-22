492ba473221f1ba368d5fcfe1f96236a6995d3b4
PaulineVarin <PaulineVarin@172.17.152.31>
2022-11-22 15:23:16 +0100

    Initialisation projet + rajout ressources

---
+++ src/petitPrince/PetitPrince0.java
@@ -0,0 +1,14 @@
+package petitPrince;
+
+import asteroide.Sujet;
+import asteroide.console.*;
+
+public class PetitPrince0 implements Sujet{
+	private String nom;
+	private int argent;
+	private ConsoleJavaBoy console ; 
+	private int cpt ; 
+	
+	public PetitPrince0
+
+}
